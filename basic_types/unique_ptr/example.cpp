603992ab2790c9d87e9b7edfee2e0d43c50f2aa1
igor.bogoslavskyi@uni-bonn.de <igor.bogoslavskyi@uni-bonn.de>
2020-01-28 12:11:09 +0100

    Update unique pointer

--- basic_types/unique_ptr/example.cpp
+++ basic_types/unique_ptr/example.cpp
@@ -14,7 +14,14 @@ struct B
 
 int main(int argc, char const *argv[])
 {
-  UniquePtr<A> ptr{new A};
-  std::cerr << "ptr: " << ptr.get() << std::endl;
+  UniquePtr<int> ptr{new int{42}};
+  UniquePtr<A> ptr_a{new A{42}};
+  UniquePtr<A> ptr_b{std::move(ptr_a)};
+  std::cerr << "ptr: " << *ptr << std::endl;
+  std::cerr << "ptr: " << *(ptr.get()) << std::endl;
+  std::cerr << "ptr: " << ptr_b->a << std::endl;
+  auto ptr_released = ptr_b.release();
+  std::cerr << "ptr: " << ptr_released->a << std::endl;
+  delete ptr_released;
   return 0;
 }
--- basic_types/unique_ptr/unique_ptr.hpp
+++ basic_types/unique_ptr/unique_ptr.hpp
@@ -1,18 +1,44 @@
-template <typename T> class UniquePtr {
+#include <memory>
+
+template <typename T, typename Deleter = std::default_delete<T>>
+class UniquePtr {
 public:
-  UniquePtr(const UniquePtr& other) = delete;
+  UniquePtr(const UniquePtr &other) = delete;
+  UniquePtr &operator=(const UniquePtr &other) = delete;
 
-  explicit UniquePtr(T *ptr) : managed_ptr_{ptr} {}
-  ~UniquePtr() { delete managed_ptr_; }
+  explicit UniquePtr(T *ptr) : managed_ptr_{ptr}, deleter_{} {}
 
-  T *get() { return managed_ptr_; }
-  void reset(T *ptr) {
-    // TODO
+  UniquePtr(UniquePtr &&other) = default;
+  UniquePtr &operator=(UniquePtr &&other) = default;
+
+  ~UniquePtr() {
+    if (managed_ptr_) {
+      deleter_(managed_ptr_);
+    }
   }
 
-  void swap(const UniquePtr &other) {}
+  T *get() { return managed_ptr_; }
+  void reset(T *ptr) {}
+
+  T *release() {
+    T *ptr = managed_ptr_;
+    managed_ptr_ = nullptr;
+    return ptr;
+  }
+
+  void swap(const UniquePtr &other) {
+    std::swap(other.managed_ptr_, managed_ptr_);
+    std::swap(other.deleter_, deleter_);
+  }
+
+  const T &operator*() const { return *managed_ptr_; }
+  T &operator*() { return *managed_ptr_; }
+
+  T *operator->() { return managed_ptr_; }
+
+  operator bool() const { return managed_ptr_ != nullptr; }
 
 private:
   T *managed_ptr_{};
+  Deleter deleter_;
 };
-
